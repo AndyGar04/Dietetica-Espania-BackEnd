246a595dd5fbf96c9e1e1e496b61dd28a77c3b6a
AndyGar04 <andygarciaprost2004@gmail.com>
2026-04-26 11:43:11 -0300

    Primeros archivos, modelo ventas sin terminar

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import 'dotenv/config';
+import Server from ".";
+// import { initializeAdmin } from "./scripts/initAdmin";
+const server = new Server(3000);
+const app = server.app;
+
+async function main() {
+    try {
+        await server.initDatabase();
+        //await initializeAdmin();
+        console.log("Base de datos lista y usuarios creados");
+    } catch (error) {
+        console.error("Error al iniciar DB:", error);
+    }
+}
+
+main();
+
+export default app;
